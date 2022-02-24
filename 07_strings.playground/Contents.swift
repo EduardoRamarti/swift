9301659553c28a8bf206d848175fcd958c9cccd6
Eduardo RM <eduardo.ramirezmar@my.unitec.edu.mx>
2022-02-23 19:20:13 -0600

    Strings

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -11,7 +11,10 @@ let multiLineString = """
 print(multiLineString)
 
 
+//Para poder usar comillas en un string
 let wiseWords = "\"La imaginación es más importante que el saber\" - Albert Einstein"
+
+//representacion de caracteres en numeros
 let dolarSign = "\u{24}"
 let blackHeart = "\u{2665}"
 let heart = "\u{1F496}"
@@ -20,18 +23,23 @@ let heart = "\u{1F496}"
 var emptyString = ""
 var anotherEmptyString = String()
 
+
+//.isEmpty sirve para saber si esta vacio
 if emptyString.isEmpty {
     print("Nada que ver aquí")
 } else{
     print("El string tiene un valor")
 }
 
+//concatenacion con operadores
 var newSomeString = "Un caballo"
 newSomeString += " y un carruaje"
 newSomeString += " y un soldado"
 
+
 let aString = "Juan Gabriel"
 //aString += " y Ricardo Celis"
+
 
 var a = "A"
 var b = "B"
@@ -45,32 +53,40 @@ print("a vale \(a) y b vale \(b) ")
 
 
 let name = "Juan Gabriel 😎"
+//imprimir cada caracter de un string con un for
 for ch in name {
     print(ch)
 }
-print(name.count)
+print(name.count) //me cuenta cuantos caracteres tiene el string name
 
 let exclamationMark : Character = "!"
 
+//creamos un array de caracteres
 let nameChars: [Character] = ["J", "u", "a", "n"]
-var nameString = String(nameChars)
+var nameString = String(nameChars) //aqui convertimos el array en un string
 
 let compoundName = "Juan " + "Gabriel"
 
-nameString.append(exclamationMark)
+nameString.append(exclamationMark) //con append agregamos al final del string lo que le indiquemos dentro del ()
 
 let multiplier = 3
 var message = "El producto de \(multiplier) x 3.5 da \(Double(multiplier)*3.5)"
 message.append(exclamationMark)
 
+
 let greeting = "Hola, ¿que tal?"
 greeting[greeting.startIndex]
-//greeting[greeting.endIndex]
+//greeting[greeting.endIndex] no podemos acceder al final del string por que con esto nos entregaria mas alla del final
+
+//aqui le decimos que el indice "antes" del final
 greeting[greeting.index(before: greeting.endIndex)]
+//aqui le decimos que el indice "despues" del que inicia
 greeting[greeting.index(after: greeting.startIndex)]
 
-for idx in greeting.indices{
-    print("\(greeting[idx])", terminator:"")
+
+for idx in greeting.indices{ //recorrera todo el string caracter por caracter
+    print("\(greeting[idx])", terminator:"")//imprime cada caracter y como terminador (alfinal de cada caracter) no hara nada
+    //por default un print tiene como terminador \n
 }
 
 
@@ -81,14 +97,23 @@ welcome.insert(contentsOf: " que tal",
                at: welcome.index(before: welcome.endIndex))
 
 welcome.remove(at: welcome.index(before: welcome.endIndex))
+
+
 welcome
+//hacemos un rango con welcome y lo siguiente se lee asi:
+//rango que que empiece al final del string de welcom menos 7 posiciones hasta uno antes del  final del string de welcome
 let range = welcome.index(welcome.endIndex, offsetBy: -7)..<welcome.endIndex
 welcome.removeSubrange(range)
 welcome
 
+
 greeting
+//aqui le estoy diciendo que establesca dentro de esta constante que el primer indice sera la ","
+//con el nil coalasing vamos a decir:
+//si el primer indice es "," entonces hazle un force unwrapping y si no establece como indice el ultimo indice
 let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
-let firstPart = greeting[..<index]
+let firstPart = greeting[..<index]//aqui le decimos que haga un rango hasta antes del index que definimos arriba
+
 
 let newString = String(firstPart)
 
@@ -96,6 +121,7 @@ let newGreeting = "Hola, soy Juan Gabriel"
 newGreeting.hasPrefix("Hola")
 newGreeting.hasSuffix("l")
 
+//creamos un Array
 let collection = [
     "Act 1 Scene 1","Act 1 Scene 2","Act 1 Scene 3","Act 1 Scene 4","Act 1 Scene 5",
     "Act 2 Scene 1","Act 2 Scene 2","Act 2 Scene 3",
